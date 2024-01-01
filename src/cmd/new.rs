92c352e6f936588442b2133d449ec5b57db0858d
Tej Singh <tejfsingh@gmail.com>
2024-01-02 00:48:02 +0530

    modify gitignore

--- src/cmd/new.rs
+++ src/cmd/new.rs
@@ -1,5 +1,7 @@
-use crate::lib::types;
+use crate::lib;
 
 pub fn create_new_instance(name: &String, size: &String, region: &String) {
-    
+    // api call to create new instance
+    let machine_id = String::from("1");
+    lib::instances::create_instance(&machine_id, name, size, region);
 }
