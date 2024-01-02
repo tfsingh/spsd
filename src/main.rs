7dd378cb54b59ddcdd05690cd09088678f9b46ca
Tej Singh <tejfsingh@gmail.com>
2024-01-02 21:07:18 +0530

    created cli module

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,11 @@
-use clap::{arg, ArgMatches, Command};
-use lib::constants::{POSSIBLE_REGIONS, POSSIBLE_SIZES};
+use clap::ArgMatches;
+
+mod cli;
 mod cmd;
 mod lib;
+
 fn main() {
-    let command = Command::new("spec")
-        .author("Tej Singh, tejfsingh@gmail.com")
-        .version("0.1")
-        .about("An interface for managing state persistent ephermeral compute")
-        .subcommand_required(true)
-        .subcommand(
-            Command::new("new")
-                .about("Create a new instance")
-                .arg(arg!(<name> "Name of instance").required(true))
-                .arg(
-                    arg!(<size> "Size of instance")
-                        .value_parser(POSSIBLE_SIZES)
-                        .required(true),
-                )
-                .arg(
-                    arg!(<region> "Region of instance")
-                        .value_parser(POSSIBLE_REGIONS)
-                        .required(true),
-                )
-                .arg_required_else_help(true),
-        )
-        .subcommand(
-            Command::new("modify")
-                .about("Modify the configuration of an instance")
-                .arg(arg!(<name> "Name of instance").required(true))
-                .arg(
-                    arg!(<size> "Size of instance")
-                        .value_parser(POSSIBLE_SIZES)
-                        .required(true),
-                )
-                .arg(
-                    arg!(<region> "Region of instance")
-                        .value_parser(POSSIBLE_REGIONS)
-                        .required(true),
-                ),
-        )
-        .subcommand(
-            Command::new("start")
-                .about("Start an instance")
-                .arg(arg!(<name> "Name of instance").required(true)),
-        )
-        .subcommand(
-            Command::new("stop")
-                .about("Stop an instance")
-                .arg(arg!(<name> "Name of instance").required(true)),
-        )
-        .subcommand(
-            Command::new("destroy")
-                .about("Destroy an instance")
-                .arg(arg!(<name> "Name of instance").required(true)),
-        )
-        .subcommand(Command::new("list").about("List all instances"))
-        .subcommand(Command::new("profile").about("Set profile for infra provider"))
-        .get_matches();
+    let command = cli::arg_parsing::read_input();
 
     match command.subcommand() {
         Some(("new", args)) => handle_command_with_all(args, |name, size, region| {
@@ -77,7 +26,7 @@ fn main() {
         Some(("list", _)) => cmd::list::list_instances(),
         Some(("profile", _args)) => cmd::profile::modify_profile(),
         _ => {
-            eprintln!("ERROR: Subcommand invalid")
+            eprintln!("Subcommand invalid")
         }
     }
 }
