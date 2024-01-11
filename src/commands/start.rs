01f1450f5d90720ff0c0912ae2c4bc99065fd86e
Tej Singh <tejfsingh@gmail.com>
2024-01-11 12:42:45 +0000

    remove unnecessary todo

--- src/commands/start.rs
+++ src/commands/start.rs
@@ -3,7 +3,6 @@ use std::process::Command;
 
 pub fn start_instance(name: &String) {
     let instance_id = utils::machines::start_machine(name).unwrap();
-    // todo: need to wait for the instance to be ready
     let mut child = Command::new("flyctl")
         .arg("ssh")
         .arg("console")
